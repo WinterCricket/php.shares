349551c3957888ae4bfd1126636b04766e4b3b7c
WinterCricket <hodhodhod2003@gmail.com>
2017-07-19 16:09:40 -0500

    connect config to actual db table

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php 
+//define db parameters
+define("DB_HOST", "localhost");
+define("DB_USE", "root");
+define("DB_PASS", "hodgoddog");
+define("DB_NAME", "shareboard");
+
+//define url
+define("ROOT_PATH", "/");
+define("ROOT_URL", "http://localhost:8080/php.shareboard");
+
+?>
